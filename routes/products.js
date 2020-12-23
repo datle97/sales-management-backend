d793f47323dc2bd598fe4678eba6fc09fdb7b7f4
datle97 <haschwalth97@gmail.com>
2020-12-23 18:12:56 +0700

    update

--- routes/products.js
+++ routes/products.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const Products = require("../models/Product");
 
 router.get("/:product", async (req, res) => {
-  const { product } = req.params;
-  const page = req.query.page >= 1 ? req.query.page : 1;
-  const limit = 40;
-  // -1 do skip bắt đầu = 0
-  const skip = (page - 1) * limit;
   try {
+    const { product } = req.params;
+    const page = req.query.page >= 1 ? req.query.page : 1;
+    const limit = 40;
+    // -1 do skip bắt đầu = 0
+    const skip = (page - 1) * limit;
     // lấy các sản phẩm theo param
     const products = await Products[product].find({}).limit(limit).skip(skip);
     res.json(products);
